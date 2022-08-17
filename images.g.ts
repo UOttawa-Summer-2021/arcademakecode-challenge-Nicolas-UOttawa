d1ff61bfe0a8c6ab5e13563f7888f14a7b3fe136
Nicolas-UOttawa <110479699+Nicolas-UOttawa@users.noreply.github.com>
2022-08-17 11:07:18 -0400

    Add directional input, button actions & scoring

--- images.g.ts
+++ images.g.ts
@@ -155,6 +155,41 @@ f 3 3 3 3 3 3 3 3 3 3 3 3 3 f .
 . . . . f f f f f f f . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
+            case "image9":
+            case "NoPress":return img`
+22ffffffffffffffffffffffffffff22
+22222222222222222222222222222222
+f222........................222f
+f2222......................2222f
+f2.222....................222.2f
+f2..222...888888888888...222..2f
+f2...2228887777777777888222...2f
+f2....22277666666666677222....2f
+f2...8822266667777666622288...2f
+f2..887622267788887762226788..2f
+f2..866662227888888922266678..2f
+f2.88666662228877882226666688.2f
+f2.88666677222677622277666688.2f
+f2.88666778822288222887766688.2f
+f2.88667788882222228888776688.2f
+f2cc866788866722227668887668cc2f
+f2cbc8668866662222666688668cbc2f
+f2cbcc86666662222226666668ccbc2f
+f2cbbcc886662226622266688ccbdc2f
+f28bbbccc88222888822288cccbddc2f
+f28cbbbbcc222cccccc222ccbdddbc2f
+f28ccbbbb222cccccccc22211dddcc2f
+f26ccccb222dddddddddd2221ddccc2f
+f26cccc222bddddddddddd222bbccc2f
+f26ccc222cccccccbbbbbbb222bccc2f
+f2f6c222cccccbbbbbbbbbbb222ccf2f
+f2f6222ccccccbbbbbbbbbbbd222cf2f
+f2f222cccccccbbbbbbbbbbbdd222f2f
+f22226cccccccbbbbbbbbbbbddb2222f
+f222ffcccccccbbbbbbbbbbbdbff222f
+22222222222222222222222222222222
+22ffffffffffffffffffffffffffff22
+`;
         }
         return null;
     })
